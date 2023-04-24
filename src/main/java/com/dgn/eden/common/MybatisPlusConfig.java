fb4162daa7dabad1a2755e74590f0cba2e7a43d1
dgnshell <douguon@163.com>
2023-04-24 16:50:25 +0800

    Add files via upload

---
+++ src/main/java/com/dgn/eden/common/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.dgn.eden.common;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Spring boot方式
+@Configuration
+@MapperScan("com.dgn.eden.mapper")
+public class MybatisPlusConfig {
+
+    // 旧版
+
+    // 最新版
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+}
+
---
+++ src/main/java/com/dgn/eden/mapper/NewsMapper.java
@@ -0,0 +1,8 @@
+package com.dgn.eden.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dgn.eden.entity.News;
+
+public interface NewsMapper extends BaseMapper<News> {
+
+}
